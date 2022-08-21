9fee37ede4efb177654d2fefbf5f53a882b9aa74
Svetlana Purple <spurple@pr-p3.kzn.21-school.ru>
2022-08-21 15:00:45 +0300

    CPP-6

--- CPP-06/ex01/main.cpp
+++ CPP-06/ex01/main.cpp
@@ -1,5 +1,28 @@
-#include "Serialization.hpp"
+#include <iostream>
+
+struct Data {
+	std::string 	first_name;
+	std::string     last_name;
+};
+
+uintptr_t serialize(Data* ptr) {
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw) {
+     return reinterpret_cast<Data *>(raw);
+}
 
 int main () {
+    Data person;
+    uintptr_t point;
+    person.first_name = "Alex";
+    person.last_name = "Ivanov";
+
+    std::cout << "Begin: " << person.first_name << " " <<person.last_name << std::endl;
+    point = serialize(&person);
+    std::cout << point << std::endl;
+    Data des_person = *deserialize(point);
+    std::cout << "After: " << des_person.first_name << " " << des_person.last_name << std::endl;
     return 1;
 }
---
+++ CPP-06/ex02/main.cpp
@@ -0,0 +1,61 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include <iostream>
+
+Base * generate(void) {
+	srand(time(NULL));
+    int rand = std::rand() % 3;
+    switch (rand) {
+        case 0:
+            std::cout << "created pointer A*" << std::endl;
+            return new A();
+        case 1:
+            std::cout << "created pointer B*" << std::endl;
+            return new B();
+        case 2:
+            std::cout << "created pointer C*" << std::endl;
+            return new C();
+    }
+    return NULL;
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "Class A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Class B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Class C" << std::endl;
+	else
+		std::cout << "No Base" << std::endl;
+}
+
+void identify(Base& p) {
+	try {
+		A instance;
+		instance = dynamic_cast<A&>(p);
+		std::cout << "&Type A" << std::endl;
+	}	catch (const std::exception&) {}
+	try {
+		B instance;
+		instance = dynamic_cast<B&>(p);
+		std::cout << "&Type A" << std::endl;
+	}	catch (const std::exception&) {}
+		try {
+		C instance;
+		instance = dynamic_cast<C&>(p);
+		std::cout << "&Type A" << std::endl;
+	}	catch (const std::exception&) {}
+}
+
+int main () {
+	Base *base;
+
+	base = generate();
+	identify(base);
+	identify(*base);
+	return 1;
+}
